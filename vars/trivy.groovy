bf96e94705e2006adbcbc9aa8eb1327fe34f0fcf
Jakub Szymczak <jakubszy@corp.idemia.com>
2024-07-10 11:54:12 +0200

    fix for trivy

--- vars/trivy.groovy
+++ vars/trivy.groovy
@@ -1,5 +1,5 @@
 def scan(imageName) {
-     sh 'docker run --name trivy_container -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.53.0 image' + imageName +' -f template --template "@contrib/html.tpl" -o /report.html'
+     sh 'docker run --name trivy_container -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.53.0 image ' + imageName +' -f template --template "@contrib/html.tpl" -o /report.html'
     sh 'docker cp trivy_container:/report.html .'
     
 }
